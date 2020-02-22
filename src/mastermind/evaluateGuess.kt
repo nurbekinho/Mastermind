ecfd821a95bf64c5102055b1e38da5e44a5a2889
Nurbek T <nurbekinho@gmail.com>
2020-02-22 23:32:31 +0100

    Implement "evaluateGame" method

--- src/mastermind/evaluateGuess.kt
+++ src/mastermind/evaluateGuess.kt
@@ -3,5 +3,35 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    return Evaluation(0, 0)
+    fun String.getRights(s: String): Int {
+        var rights = 0
+        for ((index, character) in this.withIndex()) {
+            if (s[index] == character) rights++
+        }
+
+        return rights
+    }
+
+    fun String.getWrongs(s: String): Int {
+        val sA = mutableListOf<Char>()
+        val sG = mutableListOf<Char>()
+        for ((index, character) in this.withIndex()) {
+            if (s[index] != character) {
+                sA.add(character)
+                sG.add(s[index])
+            }
+        }
+
+        var wrongs = 0
+        for (element in sA) {
+            if (element in sG) {
+                sG.remove(element)
+                wrongs++
+            }
+        }
+
+        return wrongs
+    }
+
+    return Evaluation(secret.getRights(guess), secret.getWrongs(guess))
 }
